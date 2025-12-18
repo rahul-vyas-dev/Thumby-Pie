7801e15b4fa36b98bdb076aae5f394a4e1f787f1
Rahul Vyas <rahulvyas.5100@gmail.com>
2025-12-19 00:25:27 +0530

    adding MW for proxy

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -18,6 +18,7 @@ const corsOptions = {
 
 const app: Application = express();
 
+app.set("trust proxy", 1);
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
