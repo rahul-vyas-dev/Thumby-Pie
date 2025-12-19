411a1b228a0ed37b4a6be6b2eaf6a7c112a9f716
Rahul Vyas <rahulvyas.5100@gmail.com>
2025-12-19 16:58:20 +0530

    adding multer namespace

--- BackEnd/src/types/index.d.ts
+++ BackEnd/src/types/index.d.ts
@@ -2,6 +2,29 @@ import { User } from "./User.model.type";
 import * as express from "express";
 declare global {
   namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string;
+
+        originalname: string;
+
+        encoding: string;
+
+        mimetype: string;
+
+        size: number;
+
+        stream: Readable;
+
+        destination: string;
+
+        filename: string;
+
+        path: string;
+
+        buffer: Buffer;
+      }
+    }
     interface Request {
       user?: User;
     }
