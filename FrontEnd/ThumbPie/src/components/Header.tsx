a338f962074a409e45cf3d5eee9c7f1284920a78
Rahul Vyas <rahulvyas.5100@gmail.com>
2026-01-26 01:30:35 +0530

    Revert "image as top button"
    This reverts commit 7d67015dc89be7a3eec9a6cfa873abd41737fa73.

--- FrontEnd/ThumbPie/src/components/Header.tsx
+++ FrontEnd/ThumbPie/src/components/Header.tsx
@@ -10,15 +10,13 @@ function Header() {
   const isAuthenticated = selectIsAuthenticated(User);
   return (
     <>
-      <header id="#" className="flex items-center justify-between p-4 bg-card text-foreground shadow-lg dark:shadow-gray-800 rounded-b-3xl">
+      <header className="flex items-center justify-between p-4 bg-card text-foreground shadow-lg dark:shadow-gray-800 rounded-b-3xl">
         <div>
-          <NavLink to={'#'}>
-            <img
+          <img
             src={ATG}
             alt="ATG Logo"
             className="w-11 h-11 rounded-2xl bg-transparent outline-8 hover:translate-x-0.5 hover:translate-y-0.5 blur-out ml-2 dark:invert outline-gray-300"
-            />
-          </NavLink>
+          />
         </div>
         <div className="bg-card w-1/3 h-full rounded-3xl  flex items-center justify-center px-4 py-2 shadow-lg dark:shadow-gray-700 hover:translate-x-0.5 hover:translate-y-0.5">
           <span className="flex gap-3.5 w-1/3 justify-center text-black dark:text-white">
